d343df195035f0faf6009573b208b42be975a256
KollihTheGuruh <mmike8449@gmail.com>
2023-08-09 10:45:18 -0700

    models

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Outlines the Amenity class."""
+from  models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
+
