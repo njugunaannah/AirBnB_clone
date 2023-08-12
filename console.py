9c4c860de7bddea93adca69deb07edec49edc74e
njugunaannah <njugunaannah@gmail.com>
2023-08-12 10:07:41 -0700

    edit

--- console.py
+++ console.py
@@ -60,7 +60,7 @@ class HBNBCommand(cmd.Cmd):
             "show": self.do_show,
             "destroy": self.do_destroy,
             "count": self.do_count,
-            "update": self.do_update
+            "update": self.do_update,
             "help": self.do_help,
             "quit": self.do_quit,
             "exit": self.do_quit
